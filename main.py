f7773fb6fa8549f1c14e9e30363c09403e184b94
vmargichev <93369733+vmargichev@users.noreply.github.com>
2024-02-01 10:14:42 +0200

    Completed the task

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        reading = f.read()
+        print(reading)
+    print(get_count_words(reading))
+
+def get_count_words(text):
+    words = text.split()
+    count = 0
+    for word in words:
+        count += 1
+    return count
+
+
+main()
